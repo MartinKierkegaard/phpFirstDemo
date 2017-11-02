a5bb883d28219a694c1696212958e6ba8854837a
Martin Kierkegaard <mark@easj.dk>
2017-11-02 10:35:14 +0100

    initiel commit

---
+++ student.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EASJ
+ * Date: 02/11/2017
+ * Time: 10:06
+ */
+class student(){
+    function __construct(){
+        print "in base constructor";
+    }
+}
---
+++ welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Welcome <?php echo $_POST["name"]; ?></h1><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+
+</body>
+</html>
